c33d7a99a1a593cd2e7147db8485bbbce9a2e192
Shrey Birmiwal <shreybirmiwal@Shreys-MacBook-Air.local>
2024-04-01 10:32:19 -0500

    working on accoridans

--- src/Components/Analytics/Accordian.js
+++ src/Components/Analytics/Accordian.js
@@ -13,15 +13,15 @@ export function Accordian() {
     return (
         <>
             <Accordion open={open === 1} >
-                <AccordionHeader onClick={() => handleOpen(1)}>What is Material Tailwind?</AccordionHeader>
-                <AccordionBody>
+                <AccordionHeader onClick={() => handleOpen(1)} className="text-sm pt-0">What is Material Tailwind?</AccordionHeader>
+                <AccordionBody >
                     We&apos;re not always in the position that we want to be at. We&apos;re constantly
                     growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
                     ourselves and actualize our dreams.
                 </AccordionBody>
             </Accordion>
             <Accordion open={open === 2}>
-                <AccordionHeader onClick={() => handleOpen(2)}>
+                <AccordionHeader onClick={() => handleOpen(2)} className="text-sm">
                     How to use Material Tailwind?
                 </AccordionHeader>
                 <AccordionBody>
@@ -31,7 +31,7 @@ export function Accordian() {
                 </AccordionBody>
             </Accordion>
             <Accordion open={open === 3}>
-                <AccordionHeader onClick={() => handleOpen(3)}>
+                <AccordionHeader onClick={() => handleOpen(3)} className="text-sm">
                     What can I do with Material Tailwind?
                 </AccordionHeader>
                 <AccordionBody>
--- src/Components/Analytics/AccordianOuter.js
+++ src/Components/Analytics/AccordianOuter.js
@@ -19,24 +19,16 @@ export function AccordianOuter() {
                     <Accordian />
                 </AccordionBody>
             </Accordion>
-            <Accordion open={open === 2}>
-                <AccordionHeader onClick={() => handleOpen(2)}>
-                    How to use Material Tailwind?
-                </AccordionHeader>
+            <Accordion open={open === 2} >
+                <AccordionHeader className='font-bold text-xl text-purple-600 underline mb-2' onClick={() => handleOpen(2)}>Student results</AccordionHeader>
                 <AccordionBody>
-                    We&apos;re not always in the position that we want to be at. We&apos;re constantly
-                    growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-                    ourselves and actualize our dreams.
+                    <Accordian />
                 </AccordionBody>
             </Accordion>
-            <Accordion open={open === 3}>
-                <AccordionHeader onClick={() => handleOpen(3)}>
-                    What can I do with Material Tailwind?
-                </AccordionHeader>
+            <Accordion open={open === 3} >
+                <AccordionHeader className='font-bold text-xl text-purple-600 underline mb-2' onClick={() => handleOpen(3)}>Overall analytics</AccordionHeader>
                 <AccordionBody>
-                    We&apos;re not always in the position that we want to be at. We&apos;re constantly
-                    growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-                    ourselves and actualize our dreams.
+                    <Accordian />
                 </AccordionBody>
             </Accordion>
         </>
